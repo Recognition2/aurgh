b5c10f38ab6fbcbcdba4aef44d1a0e51542cfaa8
Kevin Hill <gkh998@gmail.com>
2020-04-08 08:52:18 +0200

    Add update function

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use clap::clap_app;
 use duct::cmd;
 use itertools::Itertools;
 
+const AURTO_CACHE: &str = "/var/cache/pacman/aurto/";
+
 fn is_valid_pkg_file(s: String) -> Result<(), String> {
     if s.contains(".pkg.tar.") {
         return Ok(());
@@ -15,7 +17,7 @@ fn aurto_sync() -> Result<(), std::io::Error> {
 }
 
 // Todo: Create global lock
-fn add(pkgs: Vec<&str>, edit: bool, bind: Option<String>) {
+fn add(pkgs: Vec<&str>, edit: bool, bind: Option<String>) -> Option<()> {
     let aur_pkglist = cmd!("aur", "pkglist")
         .pipe(cmd!("sort"))
         .stdout_capture()
@@ -25,7 +27,7 @@ fn add(pkgs: Vec<&str>, edit: bool, bind: Option<String>) {
         .map(str::to_owned)
         .collect_vec();
 
-    fn aur_check_deps(needle: &str, haystack: &Vec<String>) -> Vec<String> {
+    fn aur_check_deps(needle: &str, haystack: &[String]) -> Vec<String> {
         cmd!("aur", "depends", needle)
             .stderr_null()
             .pipe(cmd!("cut", "-f2"))
@@ -68,13 +70,15 @@ fn add(pkgs: Vec<&str>, edit: bool, bind: Option<String>) {
     }
 
     pkgs_and_deps.iter().for_each(|pkg| args.push(pkg));
-    let sync = cmd("aur", &args).start().unwrap();
+    let sync = cmd("aur", &args).start().ok()?;
 
     // Wait for sync to finish
-    let out = sync.wait().unwrap();
+    let out = sync.wait().ok()?;
     if out.status.success() {
         println!("Added {} to `aurto` db successfully!", all);
     }
+    aurto_sync().ok()?;
+    Some(())
 }
 
 fn remove(pkgs: Vec<&str>) -> Option<()> {
@@ -93,19 +97,11 @@ fn remove(pkgs: Vec<&str>) -> Option<()> {
             for entry in std::fs::read_dir(dir).ok()? {
                 let entry = entry.ok()?;
                 let path = entry.path();
-                println!("Parsing path {:?}", path);
-                println!("Path is file: {}", path.is_file());
-                println!(
-                    "Path starts with {}: {}",
-                    pkg,
-                    path.file_name().unwrap().to_str().unwrap().starts_with(pkg)
-                );
-                println!("Path contains '.pkg.': {}", path.to_str().unwrap().contains(".pkg."));
                 let is_file = path.is_file();
                 let is_relevant_package = path.file_name()?.to_str()?.starts_with(pkg);
                 let contains_pkg = path.file_name()?.to_str()?.contains(".pkg.");
                 if is_file && is_relevant_package && contains_pkg {
-                    println!("Attempting to remove file");
+                    // Try to remove file
                     if std::fs::remove_file(path).is_ok() && removed_pkgs.iter().all(|&r| r != pkg) {
                         removed_pkgs.push(pkg);
                     }
@@ -119,16 +115,50 @@ fn remove(pkgs: Vec<&str>) -> Option<()> {
     Some(())
 }
 
-fn addpkg(pkgs: Vec<&str>) {
-    for pkg in pkgs {
-        println!("Updating pkg {}", pkg);
+fn addpkg(pkgs: Vec<&str>) -> Option<()> {
+    let mut args = vec!["/var/cache/pacman/aurto/aurto.db.tar"];
+    for pkg in &pkgs {
+        args.push(pkg);
     }
+
+    cmd("repo-add", args).run().ok()?;
+    for pkg in pkgs {
+        std::fs::copy(pkg, format!("{}{}", AURTO_CACHE, pkg)).ok()?;
+    }
+
+    aurto_sync().ok()?;
+    Some(())
 }
 
-fn update(pkgs: Vec<&str>, edit: bool) {
-    for pkg in pkgs {
-        println!("Updating pkg {}", pkg);
+fn update(pkgs: Vec<&str>, edit: bool, bind: Option<String>) -> Option<()> {
+    aurto_sync().ok()?;
+    let update_all = pkgs.len() == 0;
+    if update_all {
+        let mut args = vec![
+            "sync",
+            "--chroot",
+            "--database=aurto",
+            "--makepkg-conf=/etc/aurto/makepkg-chroot.conf",
+            "--upgrades",
+        ];
+
+        if !edit {
+            args.push("--no-view");
+            args.push("--no-confirm");
+        }
+
+        let s;
+        if let Some(val) = bind {
+            s = format!("--bind={}", val);
+            args.push(&s);
+        }
+        cmd("aur", args).run().ok()?;
     }
+
+
+    aurto_sync().ok()?;
+    cmd!("paccache", "-rk1", "-c", AURTO_CACHE).run().ok()?;
+    Some(())
 }
 fn main() {
     let app = clap_app!(myapp =>
@@ -143,11 +173,12 @@ fn main() {
             (about: "Add packages to `aurto` repository")
             (@arg EDIT_PKGBUILD: -e --edit "Edit PKGBUILD  before building")
             (@arg bind: --bind [dir] "Bind directory read-only")
-            (@arg packages: * "Package(s) to add")
+            (@arg packages: ... * "Package(s) to add")
         )
         (@subcommand update =>
             (about: "Try to update all packages in the `aurto` repository. Force rebuild of <packages>")
             (@arg EDIT_PKGBUILD: -e --edit "Edit PKGBUILD  before building")
+            (@arg bind: --bind [dir] "Bind directory read-only")
             (@arg packages: "Package(s) to update")
         )
         (@subcommand remove =>
@@ -167,13 +198,16 @@ fn main() {
             sub.values_of("packages").unwrap().collect(),
             sub.is_present("EDIT_PKGBUILD"),
             sub.value_of("bind").map(str::to_owned),
-        ),
+        )
+        .unwrap(),
         ("remove", Some(sub)) => remove(sub.values_of("packages").unwrap().collect()).unwrap(),
         ("update", Some(sub)) => update(
             sub.values_of("packages").unwrap().collect(),
             sub.is_present("EDIT_PKGBUILD"),
-        ),
-        ("addpkg", Some(sub)) => addpkg(sub.values_of("packages").unwrap().collect()),
+            sub.value_of("bind").map(str::to_owned),
+        )
+        .unwrap(),
+        ("addpkg", Some(sub)) => addpkg(sub.values_of("packages").unwrap().collect()).unwrap(),
 
         _ => app
             .write_help(&mut std::io::stdout())
